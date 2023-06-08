ce067d29ccaecd69793d093aaef89fe413c282d7
helox <eyes22798@gmail.com>
2023-06-08 11:22:43 +0800

    fix: example entry

--- example/main.js
+++ example/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import SvgIcon from '../src/source/index.js'
+import SvgIcon from '@eyes22798/svg-icon'
 
 Vue.config.productionTip = false
 Vue.use(SvgIcon)
