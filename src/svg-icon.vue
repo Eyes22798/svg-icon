df81d4bce3aff3070b56fac8ffda76ee2377cad2
yexinyu <yexinyu@unite.com>
2026-06-08 12:11:16 +0800

    refactor: rewrite SvgIcon to Vue 3 Composition API

--- src/svg-icon.vue
+++ src/svg-icon.vue
@@ -2,69 +2,60 @@
   <svg
     :class="svgClass"
     aria-hidden="true"
-    v-on="$listeners"
     :style="{ '--hover-color': interactHoverColor, '--active-color': interactActiveColor }"
   >
     <foreignObject class="mask-box" width="100%" height="100%">
       <div class="mask" />
-    </foreignObject>>
-    <use :xlink:href="iconName" />
+    </foreignObject>
+    <use :href="iconName" />
   </svg>
 </template>
 
-<script>
-export default {
-  name: 'SvgIcon',
-  props: {
-    name: {
-      type: String,
-      required: true
-    },
-    className: {
-      type: [String, Array],
-      default: ''
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    interact: {
-      type: Boolean,
-      default: false
-    },
-    interactHoverColor: {
-      type: String,
-      default: '#e4e5eb'
-    },
-    interactActiveColor: {
-      type: String,
-      default: '#cacdd3'
-    }
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
   },
-  computed: {
-    iconName() {
-      return `#icon-${this.name}`
-    },
-    svgClass() {
-      let svgClass = ''
-      if (this.className) {
-        svgClass = 'svg-icon' + (Array.isArray(this.className) ? this.className.join(' ') : this.className)
-      } else {
-        svgClass = 'svg-icon'
-      }
-
-      if (this.interact) {
-        svgClass += ' ' + 'interact'
-      }
-
-      if (this.disabled) {
-        svgClass += ' ' + 'disabled'
-      }
-
-      return svgClass
-    }
+  className: {
+    type: [String, Array],
+    default: ''
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  interact: {
+    type: Boolean,
+    default: false
+  },
+  interactHoverColor: {
+    type: String,
+    default: '#e4e5eb'
+  },
+  interactActiveColor: {
+    type: String,
+    default: '#cacdd3'
   }
-}
+})
+
+const iconName = computed(() => `#icon-${props.name}`)
+
+const svgClass = computed(() => {
+  let cls = 'svg-icon'
+  if (props.className) {
+    cls += ' ' + (Array.isArray(props.className) ? props.className.join(' ') : props.className)
+  }
+  if (props.interact) {
+    cls += ' interact'
+  }
+  if (props.disabled) {
+    cls += ' disabled'
+  }
+  return cls
+})
 </script>
 
 <style lang="scss">
@@ -74,15 +65,20 @@ export default {
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
+
   .mask-box {
     display: none;
   }
+
   &.interact {
     cursor: pointer;
     overflow: visible !important;
+
     .mask-box {
       position: relative;
       overflow: visible !important;
+      display: inline-block;
+
       .mask {
         position: absolute;
         width: 150%;
@@ -94,28 +90,19 @@ export default {
         top: -25%;
         box-shadow: 0px 0px 2px 0px #00000014;
       }
+    }
 
-      &:not(:disabled):hover {
-        .mask-box {
-          display: inline-block;
-        }
-        .mask {
-          background: var(--hover-color);
-        }
-      }
-      &:not(:disabled):active {
-        .mask-box {
-          display: inline-block;
-        }
-        .mask {
-          background: var(--active-color);
-        }
-      }
+    &:hover .mask {
+      background: var(--hover-color);
+    }
+
+    &:active .mask {
+      background: var(--active-color);
     }
   }
+
   &.disabled {
-    cursor: pointer;
-    border-collapse: #d9d9d9;
+    cursor: not-allowed;
     color: rgba(0, 0, 0, .25);
     box-shadow: none;
   }
