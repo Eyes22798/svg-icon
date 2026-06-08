ae9bb49c09e69aa55f7f3da9e52a5e5c0e643ce1
yexinyu <yexinyu@unite.com>
2026-06-08 12:00:51 +0800

    chore: remove Vue 2 toolchain and dependencies

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
-  ],
-  coverageDirectory: 'coverage'
-}
